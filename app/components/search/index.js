2ca6d786b58fd3be8a866ebd18d0b5624a5f6cfc
Lucas Farias <lukf95@gmail.com>
2018-03-16 11:41:01 -0300

    Refactoring fix

--- app/components/search/index.js
+++ app/components/search/index.js
@@ -1,26 +1,24 @@
 const React = require('react');
 const MeliLayout = require('../layout/');
 
-const ItemCard = () => {
-  const linkToVip = '/api/items/' + this.props.item.id;
+const ItemCard = props => {
+  const linkToVip = '/api/items/' + props.item.id;
   return (
     <div>
-      {this.props.item.title} | Precio: ${this.props.item.price.amount} |{' '}
+      {props.item.title} | Precio: ${props.item.price.amount} |{' '}
       <a href={linkToVip}>Link</a>
     </div>
   );
 };
 
-const SearchPage = () => {
-  return (
-    <MeliLayout>
-      <div>
-        {this.props.itemList.map(item => {
-          return <ItemCard item={item} key={item.id} />;
-        })}
-      </div>
-    </MeliLayout>
-  );
-};
+const SearchPage = props => (
+  <MeliLayout>
+    <div>
+      {props.itemList.map(item => {
+        return <ItemCard item={item} key={item.id} />;
+      })}
+    </div>
+  </MeliLayout>
+);
 
 module.exports = SearchPage;
